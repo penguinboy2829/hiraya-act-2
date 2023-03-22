fdd02a794150fbad21c7a834b7f40e1b658d2297
StevenGH101 <124108507+StevenGH101@users.noreply.github.com>
2023-03-22 16:08:01 +0800

    Update and rename CreateTask.js to CreateProject.js
    task change to project

--- react/reactjs/src/component/CreateProject.js
+++ react/reactjs/src/component/CreateProject.js
@@ -1,48 +1,34 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const CreateTaskPopup = ({modal, toggle, save}) => {
+const CreateProjectPopup = ({modal, toggle, save}) => {
     const [taskName, setTaskName] = useState('');
     const [description, setDescription] = useState('');
 
     const handleChange = (e) => {
-        
         const {name, value} = e.target
-
         if(name === "taskName"){
             setTaskName(value)
         }else{
             setDescription(value)
         }
-
-
     }
 
     const handleSave = (e) => {
-        //   e.preventDefault()
-        //   let taskObj = {}
-        //   taskObj["Name"] = taskName
-        //  taskObj["Description"] = description
-        //  save(taskObj)
-        fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
-      .then((data) => {
-         console.log(data);
-       
-      })
-      .catch((err) => {
-         console.log(err.message);
-      });
-
+        e.preventDefault()
+        let taskObj = {}
+        taskObj["Name"] = taskName
+        taskObj["Description"] = description
+        save(taskObj)
     }
 
     return (
         <Modal isOpen={modal} toggle={toggle}>
-            <ModalHeader toggle={toggle}>Project Task</ModalHeader>
+            <ModalHeader toggle={toggle}>Create Task</ModalHeader>
             <ModalBody>
             
                     <div className = "form-group">
-                        <label>Project Name</label>
+                        <label>Task Name</label>
                         <input type="text" className = "form-control" value = {taskName} onChange = {handleChange} name = "taskName"/>
                     </div>
                     <div>
@@ -52,11 +38,11 @@ const CreateTaskPopup = ({modal, toggle, save}) => {
                 
             </ModalBody>
             <ModalFooter>
-            <Button id="button-create"  onClick={handleSave}>Create</Button>{' '}
-            <Button id="button-cancel" onClick={toggle}>Cancel</Button>
+            <Button color="primary" onClick={handleSave}>Create</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
             </ModalFooter>
       </Modal>
     );
 };
 
-export default CreateTaskPopup;
+export default CreateProjectPopup;
