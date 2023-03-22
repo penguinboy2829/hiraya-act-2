c9ae7fae4037b95b945d7be959f36b468a0b21ca
StevenGH101 <124108507+StevenGH101@users.noreply.github.com>
2023-03-22 16:07:01 +0800

    Update and rename EditTask.js to EditProject.js
    task change to project

--- react/reactjs/src/component/EditProject.js
+++ react/reactjs/src/component/EditProject.js
@@ -1,44 +1,42 @@
 import React, { useState , useEffect} from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const EditTaskPopup = ({modal, toggle, updateTask, taskObj}) => {
-    const [taskName, setTaskName] = useState('');
+const EditProjectPopup = ({modal, toggle, updateProject, projectObj}) => {
+    const [projectName, setProjectName] = useState('');
     const [description, setDescription] = useState('');
 
     const handleChange = (e) => {
         
         const {name, value} = e.target
 
-        if(name === "taskName"){
-            setTaskName(value)
+        if(name === "projectName"){
+            setProjectName(value)
         }else{
             setDescription(value)
         }
-
-
     }
 
     useEffect(() => {
-        setTaskName(taskObj.Name)
-        setDescription(taskObj.Description)
+        setProjectName(projectObj.Name)
+        setDescription(projectObj.Description)
     },[])
 
     const handleUpdate = (e) => {
         e.preventDefault();
         let tempObj = {}
-        tempObj['Name'] = taskName
+        tempObj['Name'] = projectName
         tempObj['Description'] = description
-        updateTask(tempObj)
+        updateProject(tempObj)
     }
 
     return (
         <Modal isOpen={modal} toggle={toggle}>
-            <ModalHeader toggle={toggle}>Update Task</ModalHeader>
+            <ModalHeader toggle={toggle}>Update Project</ModalHeader>
             <ModalBody>
             
                     <div className = "form-group">
                         <label>Project Name</label>
-                        <input type="text" className = "form-control" value = {taskName} onChange = {handleChange} name = "taskName"/>
+                        <input type="text" className = "form-control" value = {projectName} onChange = {handleChange} name = "projectName"/>
                     </div>
                     <div>
                         <p>Due Date</p> 
@@ -47,11 +45,12 @@ const EditTaskPopup = ({modal, toggle, updateTask, taskObj}) => {
                 
             </ModalBody>
             <ModalFooter>
-            <Button id="button-create" onClick={handleUpdate}>Update</Button>{' '}
-            <Button id="button-cancel" onClick={toggle}>Cancel</Button>
+            <Button color="primary" onClick={handleUpdate}>Update</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
             </ModalFooter>
       </Modal>
     );
 };
 
-export default EditTaskPopup;
+export default EditProjectPopup;
+  
