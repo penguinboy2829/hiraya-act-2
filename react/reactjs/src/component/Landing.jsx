c811273104d9733f4c6c0c517ad0bfaa4b37c24a
john-frederick-yor <124217921+john-frederick-yor@users.noreply.github.com>
2023-03-27 10:26:51 +0800

    Update Landing.jsx

--- react/reactjs/src/component/Landing.jsx
+++ react/reactjs/src/component/Landing.jsx
@@ -1,14 +1,60 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from "axios"
 import { useEffect, useEffectEvent } from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
 import { Link } from "react-router-dom";
 import '../OJT.css';
 
-function Landing({}) {
-  useEffect(() => {
+function Landing() {
+  const [fname, setFname] = useState('')
+  const [lname, setLname] = useState('')
+  const [uname, setUname] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+
+  const handleFname = (event)=>{
+    setFname(event.target.value)
+  }
+  const handleLname = (event)=>{
+    setLname(event.target.value)
+  }
+
+  const handleUname = (event)=>{
+    setUname(event.target.value)
+  }
+
+  const handleEmail = (event)=>{
+    setEmail(event.target.value)
+  }
+  const handlePassword = (event)=>{
+    setPassword(event.target.value)
+  }
+
+  const handleRegister = () =>{
+    console.log (fname, lname, uname, email, password)
+    axios.post('http',{
+      fname: fname,
+      lname: lname,
+      uname: uname,
+      email: email,
+      password: password
+    })
+      .then(result => {
+      console.log(result.data)
+      alert('Sign up success')
+    })
+    .catch(error => {
+      alert('Service Error')
+      console.log(error)
+    })
+  }
+    
+   useEffect(() => {
     const signUpButton = document.getElementById('signUp');
     const signInButton = document.getElementById('signIn');
     const container = document.getElementById('container');
+   
 
     signUpButton.addEventListener('click', () => {
       container.classList.add("right-panel-active");
@@ -28,7 +74,8 @@ function Landing({}) {
         container.classList.remove("right-panel-active");
       });
     };
-  }, []); // empty dependency array to run the effect only once on mount
+  }) 
+ 
 
     return (
       <div className='col d-flex border align-items-center justify-content-center' 
@@ -49,15 +96,15 @@ function Landing({}) {
                 </a>
               </div>
               <span>or use your email for registration</span>
-              <input id="login" type="text" placeholder="First Name" />
-              <input id="login" type="text" placeholder="Last Name" />
-              <input id="login" type="text" placeholder="Username" />
-              <input id="login" type="email" placeholder="Email" />
-              <input id="login" type="password" placeholder="Password" />
-              <button className = 'button'>Sign Up</button>
+              <input className="login" onChange= {handleFname} value = {fname} type="text" placeholder="First Name" required/>
+              <input className="login" onChange= {handleLname} value = {lname} type="text" placeholder="Last Name" required/>
+              <input className="login" onChange= {handleUname} value = {uname} type="text" placeholder="Username" required/>
+              <input className="login" onChange= {handleEmail} value = {email} type="email" placeholder="Email" required/>
+              <input className="login" onChange= {handlePassword} value = {password} type="password" placeholder="Password" required/>
+              <button className = 'button' onClick={handleRegister}>Sign Up</button>
             </form>
           </div>
-
+      
           <div className="form-container sign-in-container">
             <form action="#">
               <h1>Sign in</h1>
@@ -73,12 +120,12 @@ function Landing({}) {
                 </a>
               </div>
               <span>or use your account</span>
-              <input id="login" type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
+              <input className="login"  onChange= {handleEmail} value = {email} type="email" placeholder="Email" />
+              <input type="password"  onChange= {handlePassword} value = {password} placeholder="Password" />
               <a id="forgot" href="#">Forgot your password?</a>
-              <a href = '/' className = 'button'>
+              <a href ="/" className = 'button'>
                 Log In
-              </a>
+                </a>
                 
             </form>
           </div>
@@ -88,7 +135,7 @@ function Landing({}) {
               <div className="overlay-panel overlay-left">
                 <h1>Welcome Back!</h1>
                 <p>To keep connected with us please login with your personal info</p>
-                <button className="ghost" id="signIn">
+                <button  className="ghost" id="signIn">
                   Sign In
                 </button>
               </div>
